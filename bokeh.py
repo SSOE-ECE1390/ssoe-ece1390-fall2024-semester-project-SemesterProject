65144e3c724ed7cadc0cd40b3cc4c546624e5c3c
ninjamike1211 <vandemarkm@gmail.com>
2024-09-25 13:46:55 -0400

    Update code formatting

--- bokeh.py
+++ bokeh.py
@@ -3,12 +3,18 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Read Image
 image_bgr = cv2.imread(os.path.relpath("Input/fireworks.jpg"))
 image_rgb = cv2.cvtColor(image_bgr, cv2.COLOR_BGR2RGB)
 
-image_linear = np.pow(np.float32(image_rgb)/255, 3.2)
+# Convert to linear (gamma correction, as images are stored in sRGB)
+# 2.2 is the "default" gamma, higher values produce brighter results for
+# larger radius blur
+gamma = 3.3
+image_linear = np.pow(np.float32(image_rgb)/255, gamma)
 
 
+# Generate circular kernel
 kernel_size = 21
 kernel_radius = int(kernel_size/2)
 x,y = np.indices([kernel_size, kernel_size])
@@ -16,16 +22,19 @@ x -= kernel_radius
 y -= kernel_radius
 r = np.sqrt(x**2+y**2)
 
-# kernel = r <= kernel_radius
-# kernel = (r <= kernel_radius).astype(float)
 kernel = (r <= kernel_radius).astype(float) / (kernel_size*kernel_size)
 
-# print(kernel)
-
+# Apply Bokeh Filter
 image_bokeh_linear = cv2.filter2D(image_linear, ddepth=-1, kernel=kernel)
-image_bokeh = np.pow(image_bokeh_linear, 1/3.2)
+
+# Apply inverse gamma correction
+image_bokeh = np.ubyte(np.pow(image_bokeh_linear, 1/gamma) * 255)
 
 
+# Store results to file
+cv2.imwrite("Output/fireworks_bokeh.jpg", cv2.cvtColor(image_bokeh, cv2.COLOR_RGB2BGR))
+
+# Plot results
 plt.subplot(121);plt.imshow(image_rgb)
 plt.subplot(122);plt.imshow(image_bokeh)
 plt.show()
